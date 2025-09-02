f481d7f60d217b11c4e20f51bfc2115495e4f0f9
Nyikwagh Moses <mosnyik@gmail.com>
2025-09-02 22:55:26 +0100

    fix: amde some adjustments on the landing page and change the colors on the about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,11 +14,11 @@ function About() {
       data-scroll
       data-scroll-section
       data-scroll-speed=".04"
-      className="-mt-12 bg-[#cdea68] p-6 lg:pt-14 py-20 rounded-2xl"
+      className="-mt-12 bg-zinc-900 p-6 lg:pt-14 py-20 rounded-2xl"
     >
       <div className="pb-20">
         <h2 className="custom-heading max-w-screen-xl">
-          <p className="font-NeueMontreal-Regular text-3xl md:text-5xl lg:text-5xl text-zinc-900">
+          <p className=" text-white font-NeueMontreal-Regular text-3xl md:text-5xl lg:text-5x">
             The Goldern Heart Award is an annual award ceremony organized by
             Wyte Image Media to celebrate and recognize outstanding individuals
             and organizations who have made significant contributions to various
@@ -31,23 +31,23 @@ function About() {
 
       <hr className="absolute left-0 w-full border border-t-zinc-900" />
 
-      <div className="text-zinc-900 flex flex-col gap-8 mt-6 mb-28 lg:flex-row lg:justify-between max-w-screen-sm lg:max-w-full">
+      <div className=" text-white flex flex-col gap-8 mt-6 mb-28 lg:flex-row lg:justify-between max-w-screen-sm lg:max-w-full">
         <div>How to nominate:</div>
         <div className="flex flex-col gap-8 lg:flex-row lg:w-1/2">
           <div className="flex flex-col gap-8 lg:w-1/2">
             <p>
               The Golden Heart Awards International is one of the most highly
               regarded and prestigious accolades that an individual can recieve.
-              Anyone can be nomenatted through our social media handles, 
-              but only outstanding personalities are honored.
+              Anyone can be nomenatted through our social media handles, but
+              only outstanding personalities are honored.
             </p>
             <p>
-              Anyone based anywhere in the world that meets the criterial 
-              can be nomenatted for The Golden Heart Awards. 
+              Anyone based anywhere in the world that meets the criterial can be
+              nomenatted for The Golden Heart Awards.
             </p>
             <p>
-              The Golden Heart Award International is staged annually, 
-              and can be staged in any country.
+              The Golden Heart Award International is staged annually, and can
+              be staged in any country.
             </p>
           </div>
           <div className="lg:mt-auto lg:ml-auto">
--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -65,7 +65,7 @@ function Marquee() {
       data-scroll
       data-scroll-section
       data-scroll-speed="-0.1"
-      className="-mt-4 bg-[#004d43] py-20 rounded-tr-2xl rounded-tl-2xl w-full"
+      className="-mt-4 bg-gold py-20 rounded-tr-2xl rounded-tl-2xl w-full"
     >
       <div className="border-y-2 flex overflow-hidden">
         <motion.h1
@@ -76,7 +76,7 @@ function Marquee() {
             repeat: Number.POSITIVE_INFINITY,
             duration: 10,
           }}
-          className="px-4 text-[24vw] whitespace-nowrap leading-none font-FoundersGroteskXCond-Bold uppercase pt-4 -mb-4 lg:pt-8 lg:-mb-[5.5rem] md:pt-6 md:-mb-14"
+          className=" text-zinc-900  px-4 text-[24vw] whitespace-nowrap leading-none font-FoundersGroteskXCond-Bold uppercase pt-4 -mb-4 lg:pt-8 lg:-mb-[5.5rem] md:pt-6 md:-mb-14"
         >
           {marqueeText}
         </motion.h1>
@@ -88,7 +88,7 @@ function Marquee() {
             repeat: Number.POSITIVE_INFINITY,
             duration: 10,
           }}
-          className="px-4 text-[24vw] whitespace-nowrap leading-none font-FoundersGroteskXCond-Bold uppercase pt-4 -mb-4 lg:pt-8 lg:-mb-[5.5rem] md:pt-6 md:-mb-14"
+          className=" text-zinc-900 px-4 text-[24vw] whitespace-nowrap leading-none font-FoundersGroteskXCond-Bold uppercase pt-4 -mb-4 lg:pt-8 lg:-mb-[5.5rem] md:pt-6 md:-mb-14"
         >
           {marqueeText}
         </motion.h1>
